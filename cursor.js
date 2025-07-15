d3683141ea887b3c6291d0ceadc313de96f5f8fa
lujan <169397593+paquitatorres@users.noreply.github.com>
2025-07-15 10:21:05 -0300

    Creacion de menu, portafolio tester manual , mayor responsive, botones y responsive

--- cursor.js
+++ cursor.js
@@ -8,3 +8,28 @@ document.addEventListener("mousemove", e => {
     cursor.style.left = `${e.clientX}px`;
     cursor.style.top = `${e.clientY}px`;
 });
+
+
+const hamburger= document.querySelector(".hamburger");
+const navMenu= document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click",()=>{
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n. 
+    addEventListener("click",()=>{ 
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }))
+
+
+
+
+
+
+
+
+
+
